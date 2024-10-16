5eccd039999dd4f0a2301ddbe2d58a5c6aa0ba1b
Joshua Sullivan <joshuasullivan4@mail.fresnostate.edu>
2024-10-15 17:33:28 -0700

    added basic fileserver so nick stop complaining

--- app/index.js
+++ app/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('fileserver'))
 
 app.listen(port, () => {
   console.log(`ARCHIVR is active and listing on on port ${port}`)
