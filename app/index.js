9362b4aa7af897c62a4e7077862774157308eb37
Joshua Sullivan <joshuasullivan4@mail.fresnostate.edu>
2024-10-17 17:40:13 -0700

    CHANGES

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const path = require('node:path')
+const fs = require('node:fs')
+const mysql = require('mysql2')
 const app = express()
+
 const port = 8080
 
 app.use(express.static('fileserver'))
