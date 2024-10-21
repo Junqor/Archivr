9a6e9e58f09f29f1db24f9f7c6d4d38ecf2d7787
Roberto <ramirezroberto559@gmail.com>
2024-10-20 19:53:21 -0700

    fix(back-end): close brackets

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,16 +1,16 @@
-import express from "express"
-import path from "node:path"
-import fs from "node:fs"
-import mysql from "mysql2"
+import express from "express";
+import path from "node:path";
+import fs from "node:fs";
+import mysql from "mysql2";
 import bodyParser from "body-parser";
-import {dirname} from "path";
-import {fileURLToPath} from "url";
-const app = express()
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const app = express();
 
-const port = 8080
+const port = 8080;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(bodyParser.urlencoded({ extended: true}));
-//body parser 
+app.use(bodyParser.urlencoded({ extended: true }));
+//body parser
 // nodemon
 // url and path
 
@@ -27,41 +27,39 @@ const connection = mysql.createConnection({
 
 // simple shit -- TODO: add security
 // await mySqlQuery([query]).then((rows, fields)=>data = rows[0])
-function mySqlQuery(query){
-	return new Promise(function(resolve, reject) {
-		connection.query(query, function(err, rows, fields){
-			if (err) reject(err)
-			if (rows != undefined) {
-				resolve(rows,fields)
-			}
-			else {
-				resolve(null)
-			}
-		})
-	})	
+function mySqlQuery(query) {
+  return new Promise(function (resolve, reject) {
+    connection.query(query, function (err, rows, fields) {
+      if (err) reject(err);
+      if (rows != undefined) {
+        resolve(rows, fields);
+      } else {
+        resolve(null);
+      }
+    });
+  });
 }
 
-
-app.post("/submit", (req,res) => {
+app.post("/submit", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
 
-
-app.post("/signup", (req,res) => {
+app.post("/signup", (req, res) => {
   res.sendFile(__dirname + "/public/signup/index.html");
   console.log(req.body);
   console.log(req.body["username"]);
   console.log(req.body["password"]);
 });
 
-app.post("/login", (req,res) => {
+app.post("/login", (req, res) => {
   res.sendFile(__dirname + "/public/login/index.html");
   console.log(req.body);
   console.log(req.body["username"]);
   console.log(req.body["password"]);
 });
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.listen(port, () => {
-  console.log(`ARCHIVR is active and listing on on port ${port}`)
+  console.log(`ARCHIVR is active and listing on on port ${port}`);
 });
