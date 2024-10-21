9647c42ac3058640a5af53043cca25551089133a
Roberto <ramirezroberto559@gmail.com>
2024-10-20 19:23:36 -0700

    add(ui): log in/sign up popup

--- ui/src/components/searchBar.tsx
+++ ui/src/components/searchBar.tsx
@@ -5,7 +5,7 @@ export default function SearchBar() {
     <div className="relative max-w-lg ml-5 mr-1">
       <input
         type="text"
-        className="w-full min-w-32 py-1 pl-12 pr-4 text-sm text-black rounded-[0.9rem] bg-gray focus:outline-none focus:ring-2 focus:ring-gray-400"
+        className="w-full min-w-32 py-1 pl-9 pr-4 text-sm text-black rounded-[0.9rem] bg-gray focus:outline-none focus:ring-2 focus:ring-gray-400"
       />
       <div className="absolute inset-y-0 left-0 flex items-center pl-2">
         <SearchIcon className="text-white size-5" />
---
+++ ui/src/hooks/useAuth.ts
@@ -0,0 +1,8 @@
+export const useAuth = () => {
+  return {
+    isAuthenticated: true,
+    user: {
+      name: "test",
+    },
+  };
+};
