d6234a408f5fb74d5d145924e034bb93eb6032fe
Nicholas Rodriguez <nickrodz277@gmail.com>
2024-10-22 18:04:26 -0700

    fixed enviroment variables, added create function

--- app/src/configs/sql.config.ts
+++ app/src/configs/sql.config.ts
@@ -2,9 +2,10 @@ import { validateEnv } from "../utils/validateEnv";
 import dotenv from "dotenv";
 dotenv.config();
 export const sqlConfigOptions = {
-  host: '',
-  user: '',
-  password: '',
-  database: '',
+  host:       process.env.SQL_HOST,
+  user:       process.env.SQL_USER,
+  password:   process.env.SQL_PASSWORD,
+  database:   process.env.SQL_DB,
+  port:       process.env.SQL_PORT,
   ssl: { rejectUnauthorized: true },
 };
--- app/src/index.ts
+++ app/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import path from "node:path";
 import fs from "node:fs";
 import mysql from "mysql2";
+import { ResultSetHeader } from "mysql2";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -18,33 +19,54 @@ app.use(express.static("public"));
 //body parser
 // nodemon
 // url and path
+ const connection = mysql.createConnection(sqlConfigOptions);
 
-// const connection = mysql.createConnection(sqlConfigOptions);
-
-// simple shit -- TODO: add security
-// await mySqlQuery([query]).then((rows, fields)=>data = rows[0])
-function mySqlQuery(query) {
-  return new Promise(function (resolve, reject) {
-    connection.query(query, function (err, rows, fields) {
-      if (err) reject(err);
-      if (rows != undefined) {
-        resolve(rows, fields);
-      } else {
-        resolve(null);
-      }
-    });
-  });
+// // simple shit -- TODO: add security
+ //await mySqlQuery([query]).then((rows, fields)=>data = rows[0])
+ /*
+ function mySqlQuery(query : string) {
+   return new Promise(function (resolve, reject) {
+     connection.query(query, function (err, rows, fields) {
+       if (err) reject(err);
+       if (rows != undefined) {
+         resolve(rows, fields);
+       } else {
+         resolve(null);
+       }
+     });
+   });
 }
+*/
+
+
+
+async function create(movie: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    connection.query<ResultSetHeader>(
+      "INSERT INTO Media (title) VALUES (?)",
+      [movie],
+      (err, res) => {
+        if (err) reject(err)
+        else
+          this.readById(res.insertId)
+            .then(l => resolve(l!))
+            .catch(reject)
+      }
+    )
+  })
+}
+
 
 app.post("/submit", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   res.sendFile(__dirname + "/public/signup/index.html");
   console.log(req.body);
   console.log(req.body["username"]);
   console.log(req.body["password"]);
+  await create("test");
 });
 
 app.post("/login", (req, res) => {
