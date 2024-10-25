230c06b4397d51bf7ed810201542f66ef0e22994
Joshua Sullivan <joshuasullivan4@mail.fresnostate.edu>
2024-10-24 17:27:11 -0700

    low quality rushed user sessions and log in/sign up

--- app/src/database/database.ts
+++ app/src/database/database.ts
@@ -1,8 +1,8 @@
 import {user, media} from "./databaseTypes"
 
 const database = {
-    getUserFromEmail: function (email:string){
-        if (true){
+    getUserFromEmail: async function (email:string){
+        if (email == "skibidi"){
             let user:user = {email:"grug@grugmail.com", username:"grug", password:"grug123", privilege_level:1};
             return user;
         }
@@ -10,10 +10,19 @@ const database = {
             return null;
         }
     },
-    addUser: function(user:user){
+    addUser: async function(user:user){
+        if (user.email.length > 40 || user.email.length < 0){
+            throw new Error("email size bad");
+        }
+        if (user.username.length > 40 || user.username.length < 0){
+            throw new Error("username size bad");
+        }
+        if (user.password.length > 40 || user.password.length < 0){
+            throw new Error("password size bad");
+        }
         return true;
     },
-    addMedia: function(media:media){
+    addMedia: async function(media:media){
         return true;
     },
 }
--- app/src/index.ts
+++ app/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import path from "node:path";
 import fs from "node:fs";
 import mysql from "mysql2";
+import session from "express-session";
 import { ResultSetHeader } from "mysql2";
 import bodyParser from "body-parser";
 import { dirname } from "path";
@@ -18,6 +19,11 @@ const port = 8080;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(session({
+	secret: '1',
+	resave: false,
+	saveUninitialized: true
+}))
 
 /*
 app.get("/test", async (req, res) => {
@@ -33,22 +39,91 @@ app.get("/test", async (req, res) => {
 });
 */
 
+app.get("/logout", (req, res) => {
+  if (!req.session.user){
+    res.send("You can't log out because you aren't logged in.");
+    return;
+  }
+  console.log(req.session.user.username + " logged out")
+	req.session.user = null
+	req.session.save(function(err){
+		if (err) return;
+		req.session.regenerate(function(err){
+			if (err) return;
+			res.send("you logged out")
+      return;
+		})
+	})
+})
+
+app.get("/home", (req, res) => {
+  if (!req.session.user){
+    res.send("I don't even know who you are!! What are you logged out or something!?");
+    return;
+  }
+  res.send("Oh hi, "+req.session.user.username);
+  return;
+})
+
 app.post("/submit", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/signup", async (req, res) => {
-  res.sendFile(__dirname + "/public/signup/index.html");
-  console.log(req.body);
-  console.log(req.body["username"]);
-  console.log(req.body["password"]);
+  let found_user = await database.getUserFromEmail(req.body.email);
+  if (found_user == null){
+    try{
+      let pl = 0;
+      if (req.body.admin == "on"){
+        pl = 1;
+      }
+      if (typeof(req.body.email) != "string" || typeof(req.body.username) != "string" || typeof(req.body.password) != "string"){
+        res.send("you are doing something weird stop doing that")
+        return;
+      }
+      await database.addUser({email:req.body.email,username:req.body.username,password:req.body.password,privilege_level:pl})
+      res.send("thanank you for signing up mr/mrs '"+req.body.username+"'.\ngo back and log in now and have a nice day");
+      return;
+    }
+    catch{
+      res.send("there was an error or something. go back now");
+      return;
+    }
+  }
+  else{
+    res.send("Your email used already! go back and try again");
+    return;
+  }
 });
 
-app.post("/login", (req, res) => {
-  res.sendFile(__dirname + "/public/login/index.html");
-  console.log(req.body);
-  console.log(req.body["username"]);
-  console.log(req.body["password"]);
+app.post("/login", async (req, res) => {
+  let found_user = await database.getUserFromEmail(req.body.email);
+  if (found_user == null){
+    res.send("this email is not an account go back and type a real one stupid");
+    return;
+  }
+  else{
+    if (found_user.password != req.body.password){
+      res.send("Your password is wrong! go back.");
+      return;
+    }
+    else{
+      req.session.regenerate(function(err){
+        if (err) return;
+        req.session.user = found_user;
+        req.session.save(function(err){
+          if (err) return;
+          if (req.session.user){
+            console.log(req.session.user.username + " logged in");
+            res.send("you logged in to session!!!!");
+          }
+          else{
+            res.send("something wrong happened");
+          }
+        })
+      })
+    }
+  }
 });
 
 app.listen(port, async () => {
