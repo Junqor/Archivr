4b100e489dfcdc6cafc0c5b449d6d20932ecbf5f
Roberto <ramirezroberto559@gmail.com>
2024-10-24 21:45:40 -0700

    refactor: delete html files from backend

--- app/src/index.ts
+++ app/src/index.ts
@@ -14,24 +14,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.post("/submit", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.post("/signup", async (req, res) => {
-  res.sendFile(__dirname + "/public/signup/index.html");
-  console.log(req.body);
-  console.log(req.body["username"]);
-  console.log(req.body["password"]);
-});
-
-app.post("/login", (req, res) => {
-  res.sendFile(__dirname + "/public/login/index.html");
-  console.log(req.body);
-  console.log(req.body["username"]);
-  console.log(req.body["password"]);
-});
-
 app.listen(port, () => {
   console.log(`ARCHIVR is active and listing on on port ${port}`);
 });
