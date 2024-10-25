c41d2af9e616ef64b66fd84acfea08673d21401d
Roberto <ramirezroberto559@gmail.com>
2024-10-24 22:17:28 -0700

    add(backend): add auth router

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,19 +1,20 @@
+import "./types/supabase";
 import express from "express";
-import path from "node:path";
-import fs from "node:fs";
-import mysql from "mysql2";
-import { ResultSetHeader } from "mysql2";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import "./types/supabase";
+import { authRouter } from "./routes/auth";
+
 const app = express();
 
-const port = 8080;
+const PORT = process.env.PORT || "8080";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`ARCHIVR is active and listing on on port ${port}`);
+app.use("/auth", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`ARCHIVR is active and listing on on port ${PORT}`);
 });
---
+++ app/src/routes/auth.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const authRouter = Router();
+
+authRouter.post("/signup", async (req, res) => {
+  // Do stuff
+});
+
+authRouter.post("/login", async (req, res) => {
+  // Do stuff
+});
+
+export { authRouter };
