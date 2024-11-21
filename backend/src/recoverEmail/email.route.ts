be64bf194d9bbee6ecc65c3654b2366b912fa2e6
Nicholas Rodriguez <nickrodz277@gmail.com>
2024-11-21 11:33:49 -0800

    Added to email.route.ts

--- backend/src/recoverEmail/email.route.ts
+++ backend/src/recoverEmail/email.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import  { emailVer } from "./email.js";
+import  { emailVer, setPass } from "./email.js";
 
 
 export const emailRouter = Router();
@@ -13,11 +13,24 @@ emailRouter.post("/emailVer",async (req, res) => {
         if (userEmail.error) {
           throw new Error("Invalid body");
         }
-        const email = await emailVer(userEmail); 
+        await emailVer(userEmail); 
         res.json({ status: "success" });
       } catch (error) {
         res
           .status(400)
           .json({ status: "failed", message: (error as Error).message });
       }
+});
+
+
+emailRouter.post("/passwordUpdate", async (req, res) => {
+  try{
+    const newUserPass = req.body.password; 
+    const userEmail = req.body.email; // Change so your able to get users email through the website and not input
+    await setPass(newUserPass, userEmail);
+    res.json({ status: "success" });
+  }catch(error){
+    res.status(400).json({ status: "failed", message: (error as Error).message });
+  }
+
 });
--- backend/src/recoverEmail/email.ts
+++ backend/src/recoverEmail/email.ts
@@ -1,8 +1,12 @@
 import { RowDataPacket } from "mysql2";
 import { TUser } from "../types/user.js";
 import nodemailer  from "nodemailer";
+import { generateSalt, hashPassword } from "../utils/hashPassword.js";
+import { z } from "zod";
+import { conn } from "../configs/digitalocean.config.js";
 
-const html = `<h1>Hello world</h1> <p>This is a test for nodemailer</p>`; //<--html
+const html = `<h1>Hello</h1> 
+<p>Please click this link to change your password</p>`; //<--html
 
 export async function emailVer(email: string){
 
@@ -19,11 +23,39 @@ export async function emailVer(email: string){
     const info = await transporter.sendMail({
         from: '"You" <archivrnoreply@gmail.com>',
         to: email,
-        subject: "Testing, testing, 123",
+        subject: "Changing password",
         html: html,
     });
 
     console.log("Message sent: " + info.messageId);
+}
+
+
+
+export async function setPass(inputPassword: string, email: string){
+
+  const parsedPassword = z.string().min(6).max(24).safeParse(inputPassword);
+  if (!parsedPassword.success) {
+    return {
+      status: "failed",
+      message: "Password must be between 6 and 24 characters",
+    };
+  }
+  const password = parsedPassword.data;
+
+  // Generate a random salt
+  const salt = generateSalt();
+  const { hashedPassword } = await hashPassword(password, salt);
+
+  const values = [hashedPassword, salt, email];
+
+  const query = `UPDATE Users
+  SET password_hash = (?), salt = (?)
+  WHERE email = (?)
+  `;
+
+  await conn
+  .query(query, values);
 }
 
 
