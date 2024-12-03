e81fcd085754c392640e12d26ea7b8d26d8a574c
Roberto <ramirezroberto559@gmail.com>
2024-12-02 20:11:18 -0800

    add admin check

--- ui/src/pages/admin-portal/adminPortal.tsx
+++ ui/src/pages/admin-portal/adminPortal.tsx
@@ -4,10 +4,16 @@ import { ActionButtons } from "@/pages/admin-portal/components/action-buttons";
 import { DataTable } from "@/pages/admin-portal/components/data-table";
 import { TMedia } from "@/types/media";
 import { searchMedias } from "@/api/media";
+import { useAuth } from "@/context/auth";
 
 export default function AdminPortal() {
   const [selectedItem, setSelectedItem] = useState<TMedia | null>(null); // Added state for selected items
   const [searchResults, setSearchResults] = useState<TMedia[]>([]);
+  const { user } = useAuth();
+  if (!user || user.role !== "admin") {
+    // CIA level security check fr
+    throw { status: 403 };
+  }
 
   const handleSearch = async (query: string) => {
     setSelectedItem(null);
