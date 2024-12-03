e67c6126effb4cb1cc0ebadecebce2f12b29e8cf
Roberto <ramirezroberto559@gmail.com>
2024-12-02 22:19:59 -0800

    add carousels to logged in home page for presentation

--- ui/src/pages/homePage.tsx
+++ ui/src/pages/homePage.tsx
@@ -29,20 +29,40 @@ export default function HomePage() {
         <>
           <h1>
             Welcome back,{" "}
-            <span className="text-purple font-bold">
+            <span className="font-bold text-purple">
               {user ? JSON.parse(user).username : "User"}
             </span>
             ! Here's what's new for you.
           </h1>
+          <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2">
+            <section className="flex flex-col justify-center order-2 gap-3 text-center sm:order-1">
+              <h1 className="font-extrabold leading-[normal]">
+                Track What You Love, Discover What's Next.
+              </h1>
+              <h4>
+                From your favorite classics to the latest hits, find it all in
+                one place.
+              </h4>
+            </section>
+            <section className="order-1 h-full sm:order-2">
+              <TopRatedCarousel />
+            </section>
+          </div>
           <section className="flex flex-col justify-start w-full gap-3">
             <h4 className="uppercase">Recently Reviewed...</h4>
             <RecentlyReviewed />
           </section>
+          <section className="flex flex-col justify-start w-full gap-3 pb-10">
+            <h4>Top-rated picks this week.</h4>
+            <section className="h-full">
+              <TrendingCarousel />
+            </section>
+          </section>
         </>
       ) : (
         <>
-          <div className="grid sm:grid-cols-2 grid-cols-1 gap-6 w-full">
-            <section className="flex flex-col gap-3 sm:order-1 order-2">
+          <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2">
+            <section className="flex flex-col order-2 gap-3 sm:order-1">
               <h1 className="font-extrabold leading-[normal]">
                 Track What You Love, Discover What's Next.
               </h1>
@@ -65,13 +85,13 @@ export default function HomePage() {
                 </Link>
               </div>
             </section>
-            <section className="h-full sm:order-2 order-1">
+            <section className="order-1 h-full sm:order-2">
               <TopRatedCarousel />
             </section>
           </div>
           <section className="flex flex-col justify-start w-full gap-3">
             <h4 className="uppercase">Discover on Archivr...</h4>
-            <div className="grid sm:grid-cols-3 grid-cols-2 gap-3">
+            <div className="grid grid-cols-2 gap-3 sm:grid-cols-3">
               <IconBox
                 iconName="TrendingUp"
                 description="Discover what's popular with real-time trending media across movies, shows, and more."
@@ -153,7 +173,7 @@ function RecentlyReviewed() {
   }, []);
 
   return (
-    <div className="grid sm:grid-cols-8 grid-cols-4 gap-3">
+    <div className="grid grid-cols-4 gap-3 sm:grid-cols-8">
       {media.map((item) => (
         <ThumbnailPreview key={item.id} media={item} />
       ))}
