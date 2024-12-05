1a3237294b7e932e40ab7eb7cf9e24ceeef6502c
Roberto <ramirezroberto559@gmail.com>
2024-12-04 22:29:54 -0800

    (backend) refactor env config

--- backend/src/utils/validateEnv.ts
+++
@@ -1,6 +0,0 @@
-export function validateEnv(environmentVariable: string | undefined): string {
-  if (!environmentVariable) {
-    throw new Error("Need to provide Environment Variables");
-  }
-  return environmentVariable;
-}
