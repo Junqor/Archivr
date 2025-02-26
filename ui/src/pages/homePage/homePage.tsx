839c79d5cf25035db1b978c4d514623c1740b4d3
Roberto <ramirezroberto559@gmail.com>
2025-02-26 12:18:32 -0800

    style: remove redundant element around homescreen component

--- ui/src/pages/homePage/homePage.tsx
+++ ui/src/pages/homePage/homePage.tsx
@@ -21,173 +21,170 @@ import {
 export default function HomePage() {
   const { user } = useAuth();
 
-  return (
+  return user ? (
+    // Logged in Home Screen
     <>
-      {user ? (
-        <>
-          <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
-            <section className="flex flex-col items-center justify-center gap-3 md:items-start">
-              <h1 className="font-extrabold">
-                Welcome back,{" "}
-                <span className="font-bold text-purple">{user.username}</span>
-                ! <br /> Here's what's new for you.
-              </h1>
-            </section>
-            <section className="order-1 flex h-full w-full flex-col items-center justify-center gap-3 md:order-2">
-              <StatsBox userId={user.id} />
-            </section>
-          </div>
-          <section className="flex w-full flex-col justify-start gap-3">
-            <div className="flex flex-row items-center gap-x-4">
-              <TrendingUp />
-              <h4 className="uppercase">Trending this week...</h4>
-            </div>
-            <Separator />
-            <section className="h-full overflow-visible">
-              <TrendingCarousel />
-            </section>
-          </section>
-          <SimilarCarousel />
-          <section className="flex w-full flex-col justify-start gap-3">
-            <div className="flex flex-row items-center gap-x-4">
-              <Sparkles />
-              <h4 className="uppercase">All time most popular...</h4>
-            </div>
-            <Separator />
-            <section className="h-full">
-              <MostPopularCarousel />
-            </section>
-          </section>
-          <section className="flex w-full flex-col justify-start gap-3">
-            <div className="flex flex-row items-center justify-start gap-x-4">
-              <TvRounded sx={{ fontSize: "1.71428571rem" }} />
-              <h4 className="uppercase">We think you'd love these...</h4>
-              <Popover>
-                <PopoverTrigger className="ml-auto">
-                  <Info />
-                </PopoverTrigger>
-                <PopoverContent
-                  className="inline-flex border-white text-sm text-white"
-                  side="top"
-                  align="end"
-                >
-                  <></>
-                  <p>
-                    Recommendations are based on what you like and rate.
-                    Interact with more media to get more personalized
-                    recommendations.
-                  </p>
-                </PopoverContent>
-              </Popover>
-            </div>
-            <Separator />
-            <section className="h-full">
-              <RecommendedCarousel />
-            </section>
-          </section>
-          <section className="flex w-full flex-col justify-start gap-3">
-            <div className="flex flex-row items-center gap-x-4">
-              <MessageCircleHeart />
-              <h4 className="uppercase">Recently Reviewed...</h4>
-            </div>
-            <Separator />
-            <RecentlyReviewed />
-          </section>
-        </>
-      ) : (
-        <>
-          <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
-            <section className="flex flex-col gap-3">
-              <h1 className="font-extrabold leading-[normal]">
-                Track What You Love, Discover What's Next.
-              </h1>
-              <h4>
-                From your favorite classics to the latest hits, find it all in
-                one place.
-              </h4>
-              <div className="flex flex-row gap-3">
-                <Link
-                  to="/login"
-                  className="flex items-center justify-center rounded-full bg-purple px-6 py-2 text-white transition-colors hover:bg-purple/75"
-                >
-                  Join the Community
-                </Link>
-                <Link
-                  to="#"
-                  className="flex items-center justify-center rounded-full border border-white bg-transparent px-6 py-1 transition-colors hover:bg-white hover:text-black"
-                >
-                  View the Collection
-                </Link>
-              </div>
-            </section>
-            <section className="h-full">
-              <TrendingCarousel
-                slidesPerViewMobile={3}
-                slidesPerViewDesktop={3}
-                spaceBetweenMobile={8}
-                spaceBetweenDesktop={16}
-              />
-            </section>
-          </div>
-          <section className="flex w-full flex-col justify-start gap-3">
-            <h4 className="uppercase">Discover on Archivr...</h4>
-            <div className="grid grid-cols-2 gap-3 md:grid-cols-3">
-              <IconBox
-                iconName="TrendingUp"
-                description="Discover what's popular with real-time trending media across movies, shows, and more."
-              />
-              <IconBox
-                iconName="Favorite"
-                description="Like media to boost its popularity and see how your taste impacts the trends."
-              />
-              <IconBox
-                iconName="Star"
-                description="Share your thoughts! Rate and review your favorite movies, TV shows, and books."
-              />
-              <IconBox
-                iconName="Browse"
-                description="Browse by category - movies, TV shows, books, and more - all in one place."
-              />
-              <IconBox
-                iconName="Sparkles"
-                description="Receive tailored suggestions based on what you like and rate."
-              />
-              <IconBox
-                iconName="Reviews"
-                description="Engage with the community by reading and leaving reviews on your favorite content."
-              />
-            </div>
-          </section>
-          <section className="flex w-full flex-col justify-start gap-3">
-            <div className="flex flex-row items-center space-x-4">
-              <MessageCircleHeart />
-              <h4 className="uppercase">Recently Reviewed...</h4>
-            </div>
-            <Separator />
-            <RecentlyReviewed />
-          </section>
-          <section className="flex w-full flex-col justify-start gap-3">
-            <h3>
-              Explore trending hits and hidden gems in movies, shows, music, and
-              more - just for you!
-            </h3>
-            <Separator />
-            <h4>
-              Check out top-rated picks from this week. Sign up to start
-              curating your own!
-            </h4>
-            <section className="h-full">
-              <TopRatedPicksCarousel />
-            </section>
+      <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
+        <section className="flex flex-col items-center justify-center gap-3 md:items-start">
+          <h1 className="font-extrabold">
+            Welcome back,{" "}
+            <span className="font-bold text-purple">{user.username}</span>
+            ! <br /> Here's what's new for you.
+          </h1>
+        </section>
+        <section className="order-1 flex h-full w-full flex-col items-center justify-center gap-3 md:order-2">
+          <StatsBox userId={user.id} />
+        </section>
+      </div>
+      <section className="flex w-full flex-col justify-start gap-3">
+        <div className="flex flex-row items-center gap-x-4">
+          <TrendingUp />
+          <h4 className="uppercase">Trending this week...</h4>
+        </div>
+        <Separator />
+        <section className="h-full overflow-visible">
+          <TrendingCarousel />
+        </section>
+      </section>
+      <SimilarCarousel />
+      <section className="flex w-full flex-col justify-start gap-3">
+        <div className="flex flex-row items-center gap-x-4">
+          <Sparkles />
+          <h4 className="uppercase">All time most popular...</h4>
+        </div>
+        <Separator />
+        <section className="h-full">
+          <MostPopularCarousel />
+        </section>
+      </section>
+      <section className="flex w-full flex-col justify-start gap-3">
+        <div className="flex flex-row items-center justify-start gap-x-4">
+          <TvRounded sx={{ fontSize: "1.71428571rem" }} />
+          <h4 className="uppercase">We think you'd love these...</h4>
+          <Popover>
+            <PopoverTrigger className="ml-auto">
+              <Info />
+            </PopoverTrigger>
+            <PopoverContent
+              className="inline-flex border-white text-sm text-white"
+              side="top"
+              align="end"
+            >
+              <></>
+              <p>
+                Recommendations are based on what you like and rate. Interact
+                with more media to get more personalized recommendations.
+              </p>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <Separator />
+        <section className="h-full">
+          <RecommendedCarousel />
+        </section>
+      </section>
+      <section className="flex w-full flex-col justify-start gap-3">
+        <div className="flex flex-row items-center gap-x-4">
+          <MessageCircleHeart />
+          <h4 className="uppercase">Recently Reviewed...</h4>
+        </div>
+        <Separator />
+        <RecentlyReviewed />
+      </section>
+    </>
+  ) : (
+    // Not logged in Home Screen
+    <>
+      <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
+        <section className="flex flex-col gap-3">
+          <h1 className="font-extrabold leading-[normal]">
+            Track What You Love, Discover What's Next.
+          </h1>
+          <h4>
+            From your favorite classics to the latest hits, find it all in one
+            place.
+          </h4>
+          <div className="flex flex-row gap-3">
             <Link
               to="/login"
-              className="flex w-fit items-center justify-center rounded-full bg-purple px-6 py-2 text-white transition-colors hover:bg-purple/75"
+              className="flex items-center justify-center rounded-full bg-purple px-6 py-2 text-white transition-colors hover:bg-purple/75"
             >
-              Sign Up to Discover More
+              Join the Community
             </Link>
-          </section>
-        </>
-      )}
+            <Link
+              to="#"
+              className="flex items-center justify-center rounded-full border border-white bg-transparent px-6 py-1 transition-colors hover:bg-white hover:text-black"
+            >
+              View the Collection
+            </Link>
+          </div>
+        </section>
+        <section className="h-full">
+          <TrendingCarousel
+            slidesPerViewMobile={3}
+            slidesPerViewDesktop={3}
+            spaceBetweenMobile={8}
+            spaceBetweenDesktop={16}
+          />
+        </section>
+      </div>
+      <section className="flex w-full flex-col justify-start gap-3">
+        <h4 className="uppercase">Discover on Archivr...</h4>
+        <div className="grid grid-cols-2 gap-3 md:grid-cols-3">
+          <IconBox
+            iconName="TrendingUp"
+            description="Discover what's popular with real-time trending media across movies, shows, and more."
+          />
+          <IconBox
+            iconName="Favorite"
+            description="Like media to boost its popularity and see how your taste impacts the trends."
+          />
+          <IconBox
+            iconName="Star"
+            description="Share your thoughts! Rate and review your favorite movies, TV shows, and books."
+          />
+          <IconBox
+            iconName="Browse"
+            description="Browse by category - movies, TV shows, books, and more - all in one place."
+          />
+          <IconBox
+            iconName="Sparkles"
+            description="Receive tailored suggestions based on what you like and rate."
+          />
+          <IconBox
+            iconName="Reviews"
+            description="Engage with the community by reading and leaving reviews on your favorite content."
+          />
+        </div>
+      </section>
+      <section className="flex w-full flex-col justify-start gap-3">
+        <div className="flex flex-row items-center space-x-4">
+          <MessageCircleHeart />
+          <h4 className="uppercase">Recently Reviewed...</h4>
+        </div>
+        <Separator />
+        <RecentlyReviewed />
+      </section>
+      <section className="flex w-full flex-col justify-start gap-3">
+        <h3>
+          Explore trending hits and hidden gems in movies, shows, music, and
+          more - just for you!
+        </h3>
+        <Separator />
+        <h4>
+          Check out top-rated picks from this week. Sign up to start curating
+          your own!
+        </h4>
+        <section className="h-full">
+          <TopRatedPicksCarousel />
+        </section>
+        <Link
+          to="/login"
+          className="flex w-fit items-center justify-center rounded-full bg-purple px-6 py-2 text-white transition-colors hover:bg-purple/75"
+        >
+          Sign Up to Discover More
+        </Link>
+      </section>
     </>
   );
 }
