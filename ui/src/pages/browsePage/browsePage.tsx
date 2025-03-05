49e4353a034d969977d8bfd77f7881c91dac29b4
Nicholas Rodriguez <nickrodz277@gmail.com>
2025-03-04 17:17:07 -0800

    just added browse page

--- ui/src/pages/browsePage/browsePage.tsx
+++ ui/src/pages/browsePage/browsePage.tsx
@@ -1,14 +1,3 @@
-import { useEffect } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
-import { LoadingScreen } from "../loadingScreen";
-import {
-  ChevronLeftRounded,
-  ChevronRightRounded,
-  SwapVertRounded,
-} from "@mui/icons-material";
-import { useParams, Navigate, useSearchParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-
 export default function BrowsePage() {
   return <></>;
 }
