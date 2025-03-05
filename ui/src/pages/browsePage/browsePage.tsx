c9547f6ba549c441a76485ae0f584a9216e87387
Nicholas Rodriguez <nickrodz277@gmail.com>
2025-03-05 14:48:01 -0800

    added skeleton of browse page

---
+++ ui/src/pages/browsePage/browsePage.tsx
@@ -0,0 +1,12 @@
+import SearchBar from "@/components/searchBar";
+
+export default function BrowsePage() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <div>
+        <SearchBar />
+      </div>
+    </>
+  );
+}
