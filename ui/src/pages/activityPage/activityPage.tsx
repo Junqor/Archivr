99925c6fb53d786563069c3a82788b36d261438f
Roberto <ramirezroberto559@gmail.com>
2025-03-07 10:04:44 -0800

    change: restyle activity boxes

--- ui/src/pages/activityPage/activityPage.tsx
+++ ui/src/pages/activityPage/activityPage.tsx
@@ -4,43 +4,53 @@ import { useState } from "react";
 import { ActivityFeed } from "./components/activityFeed";
 import { cn } from "@/lib/utils";
 import { RankedUserMedia } from "./components/rankedUserMedia";
+import { PersonRounded, PublicRounded } from "@mui/icons-material";
 
 export function ActivityPage() {
   const { user } = useAuth();
   const [isOnFollowing, setIsOnFollowing] = useState(!!user);
 
   return (
-    <main className="flex h-full w-full flex-col gap-y-3">
-      <div className="inline-flex items-center gap-x-2">
-        <h1 className="bg-gradient-to-tr from-blue-600 to-fuchsia-700 bg-clip-text font-extrabold">
-          See How Users Like{" "}
-          <span className="bg-clip-text text-transparent">You</span> Are Using
-          <span className="bg-clip-text text-transparent"> Archivr</span>
-        </h1>
+    <main className="flex h-full w-full flex-col gap-y-5">
+      <div className="flex flex-col gap-x-2 py-5">
+        <h1 className="font-extrabold">See What's Happening</h1>
+        <h4>
+          Discover How the{" "}
+          <span className="bg-gradient-to-tr from-blue-600 to-fuchsia-700 bg-clip-text text-transparent">
+            Archivr
+          </span>{" "}
+          Community is Exploring & Sharing
+        </h4>
       </div>
-      <div className="flex w-fit flex-row overflow-hidden rounded-md border border-white">
+      <div className="flex w-fit flex-row gap-x-2 overflow-hidden">
         {!!user && (
           <Button
             className={cn(
-              isOnFollowing ? "bg-primary" : "bg-black hover:bg-primary/40",
-              "rounded-none",
+              isOnFollowing
+                ? "bg-primary"
+                : "bg-[#1B1B1A] text-[rgba(242,_242,_240,_0.75)] hover:bg-primary/40",
+              "gap-x-2 rounded-sm py-5",
             )}
             onClick={() => setIsOnFollowing(!isOnFollowing)}
           >
-            Following
+            <PersonRounded />
+            <h4>Following</h4>
           </Button>
         )}
         <Button
           className={cn(
-            !isOnFollowing ? "bg-primary" : "bg-black hover:bg-primary/40",
-            "rounded-none",
+            !isOnFollowing
+              ? "bg-primary"
+              : "bg-[#1B1B1A] text-[rgba(242,_242,_240,_0.75)] hover:bg-primary/40",
+            "gap-x-2 rounded-sm py-5",
           )}
           onClick={() => setIsOnFollowing(!isOnFollowing)}
         >
-          Global
+          <PublicRounded />
+          <h4>Global</h4>
         </Button>
       </div>
-      <div className="flex flex-col-reverse gap-x-3 gap-y-3 md:flex-row">
+      <div className="flex flex-col-reverse gap-x-5 gap-y-5 md:flex-row">
         <section className="flex flex-col items-center gap-y-3 md:w-3/4">
           <ActivityFeed type={isOnFollowing ? "following" : "global"} />
         </section>
--- ui/src/pages/activityPage/components/activityBox.tsx
+++ ui/src/pages/activityPage/components/activityBox.tsx
@@ -2,81 +2,134 @@ import { TEnhancedActivity } from "@/api/activity";
 import { UserAvatar } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
 import { formatDate } from "@/utils/formatDate";
+import { MessageSquareQuote } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export function ActivityBox({ item }: { item: TEnhancedActivity }) {
-  const { activity, user } = item;
-  return (
-    <div className="flex min-h-20 w-full flex-col gap-y-2 rounded-xl border border-white/30 bg-slate-800 p-4">
-      <div className="flex w-full flex-row items-center gap-x-2 pb-2">
-        <UserAvatar user={user} size="small" />
-        <Link to={`/profile/${user.username}`}>
-          <h4 className="max-w-40 overflow-hidden text-ellipsis text-nowrap text-white transition-colors hover:text-purple">
-            {user.display_name || user.username}
-          </h4>
-        </Link>
-        <p className="ml-auto text-white/80">
-          {formatDate(activity.createdAt)}
-        </p>
-      </div>
-      <div className="text-white/80">
-        <ActivitySwitch item={item} />
-      </div>
-    </div>
-  );
-}
-
-// TODO: Replace usernames with link to associated member page
-function ActivitySwitch({ item }: { item: TEnhancedActivity }) {
   const { activity, media, user, review, followee } = item;
   switch (activity.activityType) {
-    case "follow":
-      return (
-        <div className="flex flex-row gap-x-2">
-          <span className="text-white">{user.username}</span> followed
-          <UserAvatar user={followee} size="small" className="inline-flex" />
-          <span className="text-white">{followee.username}</span>
-        </div>
-      );
     case "review":
       return (
-        <div className="flex flex-row gap-x-4">
+        <div className="flex w-full flex-row justify-start gap-x-4 px-3 py-4">
+          <Link to={`/profile/${user.username}`} className="h-fit w-fit">
+            <UserAvatar user={user} />
+          </Link>
           <MediaPoster media={media} className="w-20" />
-          <div className="flex flex-col">
-            <h5 className="font-bold">Reviewed {media.title}</h5>
-            <p className="line-clamp-4 overflow-hidden text-ellipsis text-white/80">
-              {review.reviewText}
-            </p>
+          <div className="flex w-full flex-col gap-y-1">
+            <div className="text-muted flex flex-row">
+              <h5>
+                <strong>{user.display_name || user.username}</strong> reviewed
+              </h5>
+              <p className="ml-auto">{formatDate(activity.createdAt)}</p>
+            </div>
+            <Link to={`/media/${media.id}`}>
+              <h4 className="font-semibold leading-tight transition-colors hover:text-primary">
+                {media.title}
+              </h4>
+            </Link>
+            <div className="flex flex-row gap-x-2">
+              <MessageSquareQuote className="mt-1 size-4 min-h-4 min-w-4" />
+              <p className="align-center line-clamp-4 overflow-hidden text-ellipsis italic text-white/80">
+                {review.reviewText}
+              </p>
+            </div>
+          </div>
+        </div>
+      );
+    case "reply":
+      return (
+        <div className="flex w-full flex-row justify-start gap-x-4 px-3 py-4">
+          <Link to={`/profile/${user.username}`} className="h-fit w-fit">
+            <UserAvatar user={user} />
+          </Link>
+          <MediaPoster media={media} className="w-20" />
+          <div className="flex w-full flex-col gap-y-1">
+            <div className="text-muted flex flex-row">
+              <h5>
+                <strong>{user.display_name || user.username}</strong> commented
+                on{" "}
+                <strong>{followee.display_name || followee.username}'s</strong>{" "}
+                review of
+              </h5>
+              <p className="ml-auto">{formatDate(activity.createdAt)}</p>
+            </div>
+            <Link to={`/media/${media.id}`}>
+              <h4 className="font-semibold leading-tight transition-colors hover:text-primary">
+                {media.title}
+              </h4>
+            </Link>
+            <div className="flex flex-row gap-x-2">
+              <p className="align-center line-clamp-4 overflow-hidden text-ellipsis italic text-white/80">
+                {activity.content}
+              </p>
+            </div>
           </div>
         </div>
       );
     case "like_review":
       return (
-        <div className="flex flex-row gap-x-4">
-          {media && <MediaPoster media={media} className="w-20" />}
-          <div className="flex flex-col gap-y-2">
-            <h5 className="font-bold">{user.username} Liked a Review:</h5>
-            <Link to={`/media/${review.mediaId}`}>
-              <p>{review.reviewText}</p>
+        <div className="flex w-full flex-row justify-start gap-x-4 px-3 py-4">
+          <Link to={`/profile/${user.username}`} className="h-fit w-fit">
+            <UserAvatar user={user} />
+          </Link>
+          <MediaPoster media={media} className="w-20" />
+          <div className="flex w-full flex-col gap-y-1">
+            <div className="text-muted flex flex-row">
+              <h5>
+                <strong>{user.display_name || user.username}</strong> liked{" "}
+                <strong>{followee.display_name || followee.username}'s</strong>{" "}
+                review of
+              </h5>
+              <p className="ml-auto">{formatDate(activity.createdAt)}</p>
+            </div>
+            <Link to={`/media/${media.id}`}>
+              <h4 className="font-semibold leading-tight transition-colors hover:text-primary">
+                {media.title}
+              </h4>
             </Link>
+            <div className="flex flex-row gap-x-2">
+              <UserAvatar user={followee} size="small" />
+              <p className="align-center line-clamp-4 overflow-hidden text-ellipsis italic text-white/80">
+                {review.reviewText}
+              </p>
+            </div>
           </div>
         </div>
       );
     case "like_media":
       return (
-        <div className="flex flex-row gap-x-4">
-          <MediaPoster media={media} className="w-20" />
-          {user.username} Liked {media.title}
+        <div className="flex flex-row gap-x-2 bg-[#1B1B1A] px-3 py-4 text-[rgba(242,_242,_240,_0.75)]">
+          <Link to={`/profile/${user.username}`}>
+            <UserAvatar user={user} size="small" />{" "}
+          </Link>
+          <p>
+            <strong>{user.display_name || user.username}</strong> liked{" "}
+            <Link
+              to={`/media/${media.id}`}
+              className="text-white transition-colors hover:text-purple"
+            >
+              <strong>{media.title}</strong>
+            </Link>
+          </p>
+          <p className="ml-auto">{formatDate(activity.createdAt)}</p>
         </div>
       );
-    case "reply":
+    case "follow":
       return (
-        <div className="flex flex-row gap-x-4">
-          <MediaPoster media={media} className="w-20" />{" "}
-          <div className="flex flex-col gap-y-2">
-            <h5 className="font-bold">Commented on {media.title}</h5>
-            <p>{activity.content}</p>
-          </div>
+        <div className="flex flex-row gap-x-2 bg-[#1B1B1A] px-3 py-4 text-[rgba(242,_242,_240,_0.75)]">
+          <Link to={`/profile/${user.username}`}>
+            <UserAvatar user={user} size="small" />{" "}
+          </Link>
+          <p>
+            <strong>{user.display_name || user.username}</strong> followed{" "}
+            <Link
+              to={`/profile/${followee.username}`}
+              className="transition-colors hover:text-purple"
+            >
+              <strong>{followee.display_name || followee.username}</strong>
+            </Link>
+          </p>
+          <p className="ml-auto">{formatDate(activity.createdAt)}</p>
         </div>
       );
     default:
